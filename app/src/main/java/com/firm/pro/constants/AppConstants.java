31c5334a40213115c278b66d1c347b91314735f9
von2018 <erchashu@126.com>
2025-07-26 12:42:30 +0800

    second

--- app/src/main/java/com/firm/pro/constants/AppConstants.java
+++ app/src/main/java/com/firm/pro/constants/AppConstants.java
@@ -20,15 +20,60 @@ public class AppConstants {
      */
     public static class AdType {
         public static final int DEFAULT = -1;
+        /**
+         * 原生广告类型ID
+         */
         public static final int NATIVE = 0;
+        /**
+         * 激励视频广告类型ID
+         */
         public static final int REWARDED_VIDEO = 1;
+        /**
+         * 横幅广告类型ID
+         */
         public static final int BANNER = 2;
+        /**
+         * 插屏广告类型ID
+         */
         public static final int INTERSTITIAL = 3;
+        /**
+         * 开屏广告类型ID
+         */
         public static final int SPLASH = 4;
         public static final int MEDIA_VIDEO = 50;
 
         public AdType() {
         }
+    }
+
+    /**
+     * 广告平台ID
+     */
+    public static class NetworkFirmId {
+        /**
+         * 腾讯广告网络ID
+         */
+        public static final int TENCENT_ADS = 8;
+
+        /**
+         * 穿山甲广告网络ID
+         */
+        public static final int CSJ = 15;
+
+        /**
+         * 百度联盟广告网络ID
+         */
+        public static final int BAIDU_UNION_ADS = 22;
+
+        /**
+         * 快手广告网络ID
+         */
+        public static final int KUASHOU_ADS = 28;
+
+        public NetworkFirmId() {
+        }
+
+
     }
 
     public static class AdDismissType {
